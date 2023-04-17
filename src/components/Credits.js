39549d08beea164a489c96035c2181c4bff85007
Nick Melkadze <25963683+melkadze@users.noreply.github.com>
2023-04-16 22:30:13 -0400

    show balances on all pages  (credits and debits)

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -4,12 +4,17 @@ src/components/Credits.js
 The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
+
+// Import Link as well as the balance component
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Credits = (props) => {
   return (
     <div>
       <h1>Credits</h1>
+      <br/>
+      <AccountBalance accountBalance={props.accountBalance}/>
       <br/>
       <Link to="/">Return to Home</Link>
     </div>
